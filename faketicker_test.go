ec4c2d55277902d1b88f00a975d9e635f16c3b9d
Songmu <y.songmu@gmail.com>
2020-08-30 22:31:47 +0900

    add test for fakeTicker.Reset

--- faketicker_test.go
+++ faketicker_test.go
@@ -8,6 +8,16 @@ import (
 )
 
 func TestFakeTicker_Reset(t *testing.T) {
+	now := time.Now()
+	restore := flextime.Fix(now)
+	defer restore()
+	ti := flextime.NewTicker(time.Second)
+	ti.Reset(10 * time.Millisecond)
+	expect := now.Add(1*time.Second + 10*time.Millisecond)
+	almostSameTime(t, <-ti.C, expect)
+}
+
+func TestFakeTicker_Reset_panic(t *testing.T) {
 	restore := flextime.Fix(time.Now())
 	expect := "non-positive interval for NewTicker"
 	defer func() {
